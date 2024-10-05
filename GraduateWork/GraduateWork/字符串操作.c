cf727542d505263b048fb78ebbb56c33ae8e714d
HUYONGLONG <2830915693@qq.com>
2024-10-05 21:39:56 +0800

    1

--- GraduateWork/GraduateWork/字符串操作.c
+++ GraduateWork/GraduateWork/字符串操作.c
@@ -143,8 +143,8 @@ int strcmp(const char* str1, const char* str2)
 //	return 0;
 //}
 
-int main()
-{
+//int main()
+//{
 	/*struct A
 	{
 		int _a : 2;
@@ -162,16 +162,77 @@ int main()
 	//};
 	//enum Color clr = GREEN;//只能拿枚举常量给枚举变量赋值，才不会出现类型的差异。
 
-	union Un
+//	union Un
+//	{
+//		char c;
+//		int i;
+//	};
+//	//联合变量的定义
+//	union Un un;
+//	//计算连个变量的大小
+//	printf("%d\n", sizeof(un));
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//代码1
+//	int num = 0;
+//	scanf("%d", &num);
+//	int arr[3] = { 0 };
+//	//代码2
+//	int* ptr = NULL;
+//	ptr = (int*)malloc(num * sizeof(int));
+//	if (NULL != ptr)//判断ptr指针是否为空
+//	{
+//		int i = 0;
+//		for (i = 0; i < num; i++)
+//		{
+//			*(ptr + i) = 0;
+//		}
+//	}
+//	free(ptr);//释放ptr所指向的动态内存
+//	ptr = NULL;//是否有必要？
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* p = calloc(10, sizeof(int));
+//	if (NULL != p)
+//	{
+//		//使用空间
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+int main()
+{
+	int* ptr = malloc(100);
+	if (ptr != NULL)
 	{
-		char c;
-		int i;
-	};
-	//联合变量的定义
-	union Un un;
-	//计算连个变量的大小
-	printf("%d\n", sizeof(un));
+		//业务处理
+	}
+	else
+	{
+		//exit(EXIT_FAILURE);
+	}
+	//扩展容量
+	//代码1
+	ptr = realloc(ptr, 1000);
 
-
+	//代码2
+	int* p = NULL;
+	p = realloc(ptr, 1000);
+	if (p != NULL)
+	{
+		ptr = p;
+	}
+	//业务处理
+	free(ptr);
 	return 0;
 }
