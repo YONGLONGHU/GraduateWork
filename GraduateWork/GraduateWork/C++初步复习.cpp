b4a1868d998d11480a47e489fe5e6769de104111
HUYONGLONG <2830915693@qq.com>
2024-10-08 12:00:11 +0800

    1

--- GraduateWork/GraduateWork/C++初步复习.cpp
+++ GraduateWork/GraduateWork/C++初步复习.cpp
@@ -1,75 +1,75 @@
 #include<iostream>
 using namespace std;
 //命名空间，避免命名冲突或名字污染
-namespace N
-{
-	int a;
-	int Add(int left, int right)
-	{
-		return left + right;
-	}
-}
-
-//2. 命名空间可以嵌套
-namespace N2
-{
-	int a;
-	int b;
-	int Add(int left, int right)
-	{
-		return left + right;
-	}
-
-	namespace N3
-	{
-		int c;
-		int d;
-		int Sub(int left, int right)
-		{
-			return left - right;
-		}
-	}
-}
-
-//3. 同一个工程中允许存在多个相同名称的命名空间,编译器最后会合成同一个命名空间中。
-namespace N1
-{
-	int Mul(int left, int right)
-	{
-		return left * right;
-	}
-}
-
-using N::a;
-int main1()
-{
-	printf("%d\n", N::a);
-	printf("%d\n", a);
-	return 0;
-}
-using namespace N;
-
-int main2()
-{
-	printf("%d\n", N::a);
-	printf("%d\n", a);
-	Add(10, 20);
-	return 0;
-}
-
-int main3()
-{
-	int a;
-	double b;
-	char c;
-	cin >> a;
-	cin >> b >> c;
-
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
-	return 0;
-}
+//namespace N
+//{
+//	int a;
+//	int Add(int left, int right)
+//	{
+//		return left + right;
+//	}
+//}
+//
+////2. 命名空间可以嵌套
+//namespace N2
+//{
+//	int a;
+//	int b;
+//	int Add(int left, int right)
+//	{
+//		return left + right;
+//	}
+//
+//	namespace N3
+//	{
+//		int c;
+//		int d;
+//		int Sub(int left, int right)
+//		{
+//			return left - right;
+//		}
+//	}
+//}
+//
+////3. 同一个工程中允许存在多个相同名称的命名空间,编译器最后会合成同一个命名空间中。
+//namespace N1
+//{
+//	int Mul(int left, int right)
+//	{
+//		return left * right;
+//	}
+//}
+//
+//using N::a;
+//int main1()
+//{
+//	printf("%d\n", N::a);
+//	printf("%d\n", a);
+//	return 0;
+//}
+//using namespace N;
+//
+//int main2()
+//{
+//	printf("%d\n", N::a);
+//	printf("%d\n", a);
+//	Add(10, 20);
+//	return 0;
+//}
+//
+//int main3()
+//{
+//	int a;
+//	double b;
+//	char c;
+//	cin >> a;
+//	cin >> b >> c;
+//
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//	return 0;
+//}
 
 //缺省参数
 //void TestFunc(int a = 0)
@@ -100,21 +100,21 @@ int main3()
 //}
 
 //引用
-void TestRse()
-{
-	int a = 0;
-	int& b = a;
-
-	cout << &a << endl;
-	cout << &b << endl;
-}
-
-void swap(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
+//void TestRse()
+//{
+//	int a = 0;
+//	int& b = a;
+//
+//	cout << &a << endl;
+//	cout << &b << endl;
+//}
+//
+//void swap(int& a, int& b)
+//{
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//}
 
 //int& Add1(int a, int b)
 //{
@@ -142,3 +142,74 @@ void swap(int& a, int& b)
 //	return 0;
 //}
 
+//struct student
+//{
+//	void SetStu(const char* _name, const char* _gender, int _age)
+//	{
+//		strcpy(name, _name);
+//		strcpy(gender, _gender);
+//		age = _age;
+//	}
+//	void PrintStu()
+//	{
+//		cout << age << " " << gender << " " << name << endl;
+//	}
+//
+//	int age;
+//	char gender[3];
+//	char name[20];
+//};
+//
+//class Date
+//{
+//public:
+//	void Display()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//
+//	void SetDate(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year; // 年
+//	int _month; // 月
+//	int _day;
+//};
+//int main6()
+//{
+//	Date d1, d2;
+//	d1.SetDate(2018, 5, 1);
+//	d2.SetDate(2018, 7, 1);
+//	d1.Display();
+//	d2.Display();
+//	return 0;
+//}
+//
+//class A
+//{
+//public:
+//	void PrintA()
+//	{
+//		cout << _a << endl;
+//	}
+//
+//	void Show()
+//	{
+//		cout << "Show()" << endl;
+//	}
+//private:
+//	int _a;
+//};
+//int main7()
+//{
+//	A* p = NULL;
+//	p->PrintA();
+//	p->Show();
+//	return 0;
+//}
+//
+//
---
+++ GraduateWork/GraduateWork/类的默认成员函数.cpp
@@ -0,0 +1,395 @@
+#include<iostream>
+#include <cassert>
+using namespace std;
+//析构函数
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//
+//	}
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//void TestDate()
+//{
+//	Date d2(2019, 8, 30);
+//	// 注意：如果通过无参构造函数创建对象时，对象后面不用跟括号，否则就成了函数声明
+//	// 以下代码的函数：声明了d3函数，该函数无参，返回一个日期类型的对象
+//	Date d3();
+//
+//}
+//// 默认构造函数
+//class Date1
+//{
+//public:
+//	Date1()
+//	{
+//		_year = 1900;
+//		_month = 1;
+//		_day = 1;
+//	}
+//
+//	Date1(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//// 以下测试函数能通过编译吗？
+////void TestDate1()
+////{
+////	Date1 d1;//默认构造函数有两个了，所以编译不通过，只能有一个
+////}
+//
+////析构函数
+//typedef int DataType;
+//class SeqList
+//{
+//public:
+//	SeqList(int capacity=10)
+//	{
+//		_pData = (DataType*)malloc(capacity * sizeof(DataType));
+//		assert(_pData);
+//		_size = 0;
+//		_capacity = capacity;
+//	}
+//	~SeqList()
+//	{
+//		if (_pData)
+//		{
+//			free(_pData);
+//			_pData = nullptr;
+//			_capacity = 0;
+//			_size = 0;
+//		}
+//	}
+//private:
+//	int* _pData;
+//	size_t _capacity;
+//	size_t _size;
+//};
+////析构函数可以清理自定义的变量空间
+//class String
+//{
+//public:
+//	String(const char* str = "hello")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//	}
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//
+////拷贝构造函数
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Date(Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main8()
+//{
+//	Date d1(1990,4,28);
+//	Date d2(d1);
+//	return 0;
+//}
+//
+//class String
+//{
+//public:
+//	String(const char* str = "jack")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//	}
+//
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//int main10()
+//{
+//	String s1("hello");
+//	String s2(s1);
+//}
+//
+////赋值运算符重载
+//class Date4
+//{
+//public:
+//	Date4(int year = 1900, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//bool operator==(const Date4& d1, const Date4& d2)
+//{
+//	return d1._year == d2._year
+//	&& d1._month == d2._month
+//		&& d1._day == d2._day;
+//}
+//
+//void TestOperator()
+//{
+//	Date4 d1(2005, 2, 23);
+//	Date4 d2(2005, 2, 23);
+//	cout << (d1 == d2) << endl;
+//}
+//
+////class Date
+////{
+////public:
+////	Date(int year = 1900, int month = 1, int day = 1)
+////	{
+////		_year = year;
+////		_month = month;
+////		_day = day;
+////	}
+////
+////	// bool operator==(Date* this, const Date& d2)
+////	// 这里需要注意的是，左操作数是this指向的调用函数的对象
+////	bool operator==(const Date& d2)
+////	{
+////		_year = d2._year;
+////		_month = d2._month;
+////		_day = d2._day;
+////	}
+////private:
+////	int _year;
+////	int _month;
+////	int _day;
+////};
+////int main()
+////{
+////	Date d1(1990.2,23);
+////	Date d2(1991,2,23);
+////	cout << (d1 == d2) << endl;
+////	return 0;
+////}
+//
+////class String1
+////{
+////public:
+////	String1(const char* str = "")
+////	{
+////		_str = (char*)malloc(strlen(str) + 1);
+////		strcpy(_str, str);
+////	}
+////
+////	~String1()
+////	{
+////		cout << "~String()" << endl;
+////		free(_str);
+////	}
+////private:
+////	char* _str;
+////};
+////
+////int main11()
+////{
+////	String1 s1("hello");
+////	String1 s2("world");
+////
+////	s1 = s2;
+////}
+////class A
+////{
+////public:
+////	A(int a)
+////		:_a(a)
+////	{}
+////private:
+////	int _a;
+////};
+//
+//class B
+//{
+//public:
+//	B(int a, int ref)
+//		:_aobj(a)
+//		, _ref(ref)
+//		, _n(10)
+//	{}
+//private:
+//	A _aobj; // 没有默认构造函数
+//	int& _ref; // 引用
+//	const int _n; // const 
+//};
+
+//class A
+//{
+//public:
+//	A(int a)
+//		:_a1(a)
+//		, _a2(_a1)
+//	{}
+//
+//	void Print() {
+//		cout << _a1 << " " << _a2 << endl;
+//	}
+//private:
+//	int _a2;
+//	int _a1;
+//};
+//int main1() {
+//	A aa(1);
+//	aa.Print();
+//}
+//class Date
+//{
+//public:
+//	Date(int year)
+//		:_year(year)
+//	{}
+//
+//	explicit Date(int year)
+//		:_year(year)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//void TestDate()
+//{
+//	Date d1(2018);
+//	// 用一个整形变量给日期类型对象赋值
+//	 // 实际编译器背后会用2019构造一个无名对象，最后用无名对象给d1对象进行赋值
+//	d1 = 2019;
+//}
+////Static成员变量及成员函数
+//class A
+//{
+//public:
+//	A()
+//	{
+//		++_Count;
+//	}
+//	A(const A& t)
+//	{
+//		++_Count;
+//	}
+//	static int GetAcount()
+//	{
+//		return _Count;
+//	}
+//private:
+//	static int _Count;
+//};
+//int A::_Count = 0;
+
+//友元函数
+//class Date1
+//{
+//public:
+//	Date1(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//
+//	ostream& operator<<(ostream& _cout)
+//	{
+//		_cout << _year << "-" << _month << "-" << _day;
+//		return _cout;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date1 d(2017, 12, 24);
+//	d << cout;
+//	d.operator<<(cout);
+//	return 0;
+//}
+
+class Date
+{
+	friend ostream& operator<<(ostream& _cout, const Date& d);
+	friend istream& operator>>(istream& _cin, Date& d);
+public:
+	Date(int year, int month, int day)
+		: _year(year)
+		, _month(month)
+		, _day(day)
+	{}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+ostream& operator<<(ostream& _cout, const Date& d)
+{
+	_cout << d._year << "-" << d._month << "-" << d._day;
+
+	return _cout;
+}
+
+istream& operator>>(istream& _cin, Date& d)
+{
+	_cin >> d._year;
+	_cin >> d._month;
+	_cin >> d._day;
+
+	return _cin;
+}
+int main()
+{
+	Date d(2019,2,33);
+	cin >> d;
+	cout << d << endl;
+	return 0;
+}
