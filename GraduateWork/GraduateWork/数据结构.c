d184663b75b1553858ffd2dd24e314490787c68a
HUYONGLONG <2830915693@qq.com>
2024-11-05 16:20:58 +0800

    1

--- GraduateWork/GraduateWork/数据结构.c
+++ GraduateWork/GraduateWork/数据结构.c
@@ -218,3 +218,115 @@
 //        return false;
 //    }
 //};
+//class Solution {
+//public:
+//    int findDuplicate(vector<int>& nums)
+//    {
+//        int length = nums.size();
+//        for (int i = 0; i < length - 1; i++)
+//        {
+//            for (int j = i + 1; j < length; j++)
+//            {
+//                if (nums[i] == nums[j])
+//                    return nums[i];
+//            }
+//        }
+//        return nums[length - 1];
+//    }
+//};
+//class Solution {
+//public:
+//    int findDuplicate(vector<int>& nums)
+//    {
+//        sort(nums.begin(), nums.end());
+//        for (int i = 0, j = 1; i < nums.size() - 1; i++, ++j)
+//        {
+//            if (nums[i] == nums[j]) return nums[j];
+//        }
+//        return nums[0];
+//    }
+//};
+//class Solution {
+//public:
+//    int maxArea(vector<int>& height)
+//    {
+//        int left = 0;
+//        int right = height.size() - 1;
+//        int result = 0;
+//        while (left < right)
+//        {
+//            int sum = min(height[right], height[left]) * (right - left);
+//            result = max(sum, result);
+//            if (height[left] < height[right])
+//                left++;
+//            else
+//                right--;
+//        }
+//        return result;
+//    }
+//};
+
+//#pragma once
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <cassert>
+//#include <string>
+//
+//#include <unistd.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+//typedef struct sockaddr sockaddr;
+//typedef struct sockaddr_in sockaddr_in;
+//
+//class UdpSocket {
+//public:
+//	UdpSocket() : fd_(-1) {
+//	}
+//	bool Socket() {
+//		fd_ = socket(AF_INET, SOCK_DGRAM, 0);
+//		if (fd_ < 0) {
+//			perror("socket");
+//			return false;
+//		}
+//		return true;
+//	}
+//
+//	bool Close() {
+//		close(fd_);
+//		return true;
+//	}
+//
+//	bool Bind(const std::string& ip, uint16_t port) {
+//		sockaddr_in addr;
+//		addr.sin_family = AF_INET;
+//		addr.sin_addr.s_addr = inet_addr(ip.c_str());
+//		addr.sin_port = htons(port);
+//		int ret = bind(fd_, (sockaddr*)&addr, sizeof(addr));
+//		if (ret < 0) {
+//			perror("bind");
+//			return false;
+//		}
+//		return true;
+//	}
+//	bool RecvFrom(std::string* buf, std::string* ip = NULL, uint16_t* port = NULL) {
+//		char tmp[1024 * 10] = { 0 };
+//		sockaddr_in peer;
+//		socklen_t len = sizeof(peer);
+//		ssize_t read_size = recvfrom(fd_, tmp,
+//			sizeof(tmp) - 1, 0, (sockaddr*)&peer, &len);
+//		if (read_size < 0) {
+//			perror("recvfrom");
+//			return false;
+//		}
+//		// 将读到的缓冲区内容放到输出参数中
+//		buf->assign(tmp, read_size);
+//		if (ip != NULL) {
+//			*ip = inet_ntoa(peer.sin_addr);
+//		}
+//		if (port != NULL) {
+//			*port = ntohs(peer.sin_port);
+//		}
+//		return true;
+//	}
