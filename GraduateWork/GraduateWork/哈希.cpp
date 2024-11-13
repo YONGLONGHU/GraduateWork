fb1ffa0ffaa3ac642de25663abf660f8cb2f78e8
HUYONGLONG <2830915693@qq.com>
2024-11-13 21:09:51 +0800

    1

--- GraduateWork/GraduateWork/哈希.cpp
+++ GraduateWork/GraduateWork/哈希.cpp
@@ -702,65 +702,65 @@ using namespace std;
 //    }
 //};
 
-#include <vector>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result = { -1, -1 }; // 初始化结果为 [-1, -1]
-
-        // 查找开始位置
-        result[0] = findFirstPosition(nums, target);
-        // 查找结束位置
-        result[1] = findLastPosition(nums, target);
-
-        return result;
-    }
-
-private:
-    int findFirstPosition(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        int firstPos = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                firstPos = mid; // 找到目标值，记录位置
-                right = mid - 1; // 继续查找左侧
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-
-        return firstPos;
-    }
-
-    int findLastPosition(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        int lastPos = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                lastPos = mid; // 找到目标值，记录位置
-                left = mid + 1; // 继续查找右侧
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-
-        return lastPos;
-    }
-};
+//#include <vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//    vector<int> searchRange(vector<int>& nums, int target) {
+//        vector<int> result = { -1, -1 }; // 初始化结果为 [-1, -1]
+//
+//        // 查找开始位置
+//        result[0] = findFirstPosition(nums, target);
+//        // 查找结束位置
+//        result[1] = findLastPosition(nums, target);
+//
+//        return result;
+//    }
+//
+//private:
+//    int findFirstPosition(vector<int>& nums, int target) {
+//        int left = 0, right = nums.size() - 1;
+//        int firstPos = -1;
+//
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] == target) {
+//                firstPos = mid; // 找到目标值，记录位置
+//                right = mid - 1; // 继续查找左侧
+//            }
+//            else if (nums[mid] < target) {
+//                left = mid + 1;
+//            }
+//            else {
+//                right = mid - 1;
+//            }
+//        }
+//
+//        return firstPos;
+//    }
+//
+//    int findLastPosition(vector<int>& nums, int target) {
+//        int left = 0, right = nums.size() - 1;
+//        int lastPos = -1;
+//
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] == target) {
+//                lastPos = mid; // 找到目标值，记录位置
+//                left = mid + 1; // 继续查找右侧
+//            }
+//            else if (nums[mid] < target) {
+//                left = mid + 1;
+//            }
+//            else {
+//                right = mid - 1;
+//            }
+//        }
+//
+//        return lastPos;
+//    }
+//};
 
 // 示例用法
 // int main() {
