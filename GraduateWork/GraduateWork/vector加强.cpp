5ff93dae48595129a95b8e583804feafb41884ec
HUYONGLONG <2830915693@qq.com>
2024-12-19 10:34:13 +0800

    归并排序

--- GraduateWork/GraduateWork/vector加强.cpp
+++ GraduateWork/GraduateWork/vector加强.cpp
@@ -1435,3 +1435,80 @@ public:
 //
 //	return 0;
 //}
+//#include <iostream>
+//#include <vector>
+//
+//// 合并两个有序子数组
+//void merge(std::vector<int>& arr, int left, int mid, int right) {
+//    int n1 = mid - left + 1; // 左子数组的长度
+//    int n2 = right - mid;   // 右子数组的长度
+//
+//    // 创建临时数组
+//    std::vector<int> L(n1), R(n2);
+//
+//    // 拷贝数据到临时数组 L[] 和 R[]
+//    for (int i = 0; i < n1; i++)
+//        L[i] = arr[left + i];
+//    for (int j = 0; j < n2; j++)
+//        R[j] = arr[mid + 1 + j];
+//
+//    // 合并临时数组回到 arr[left..right]
+//    int i = 0; // 初始索引第一个子数组
+//    int j = 0; // 初始索引第二个子数组
+//    int k = left; // 初始索引合并子数组
+//
+//    while (i < n1 && j < n2) {
+//        if (L[i] <= R[j]) {
+//            arr[k] = L[i];
+//            i++;
+//        }
+//        else {
+//            arr[k] = R[j];
+//            j++;
+//        }
+//        k++;
+//    }
+//
+//    // 拷贝 L[] 的剩余元素
+//    while (i < n1) {
+//        arr[k] = L[i];
+//        i++;
+//        k++;
+//    }
+//
+//    // 拷贝 R[] 的剩余元素
+//    while (j < n2) {
+//        arr[k] = R[j];
+//        j++;
+//        k++;
+//    }
+//}
+//
+//// 归并排序函数
+//void mergeSort(std::vector<int>& arr, int left, int right) {
+//    if (left < right) {
+//        // 同一半数组找中间位置
+//        int mid = left + (right - left) / 2;
+//
+//        // 分别对左右两半进行归并排序
+//        mergeSort(arr, left, mid);
+//        mergeSort(arr, mid + 1, right);
+//
+//        // 合并两个有序半数组
+//        merge(arr, left, mid, right);
+//    }
+//}
+//
+//int main() {
+//    std::vector<int> arr = { 12, 11, 13, 5, 6, 7 };
+//    int arr_size = arr.size();
+//
+//    mergeSort(arr, 0, arr_size - 1);
+//
+//    std::cout << "Sorted array: \n";
+//    for (int i = 0; i < arr_size; i++)
+//        std::cout << arr[i] << " ";
+//    std::cout << std::endl;
+//
+//    return 0;
+//}
