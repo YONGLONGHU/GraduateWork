88c0cbaadb23244692b78bb25a74b71e208802f0
HUYONGLONG <2830915693@qq.com>
2024-12-23 11:39:45 +0800

    2

--- GraduateWork/GraduateWork/力扣练习题.cpp
+++ GraduateWork/GraduateWork/力扣练习题.cpp
@@ -83,3 +83,17 @@
 //    std::cout << fractionToDecimal(numerator, denominator) << std::endl;
 //    return 0;
 //}
+// excel±íÁĞÃû³Æ
+//class Solution {
+//public:
+//    string convertToTitle(int columnNumber) {
+//        string ans;
+//        while (columnNumber > 0) {
+//            int a0 = (columnNumber - 1) % 26 + 1;
+//            ans += a0 - 1 + 'A';
+//            columnNumber = (columnNumber - a0) / 26;
+//        }
+//        reverse(ans.begin(), ans.end());
+//        return ans;
+//    }
+//};
