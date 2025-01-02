ec776df9faa8d6a5eb38e2d543134fd173f6ad6d
HUYONGLONG <2830915693@qq.com>
2025-01-02 11:01:32 +0800

    翻转二叉树

--- GraduateWork/GraduateWork/力扣练习题.cpp
+++ GraduateWork/GraduateWork/力扣练习题.cpp
@@ -261,3 +261,17 @@
 //        return false;
 //    }
 //};
+//·´×ª¶þ²æÊ÷
+//class Solution {
+//public:
+//    TreeNode* invertTree(TreeNode* root) {
+//        if (root == nullptr) {
+//            return nullptr;
+//        }
+//        TreeNode* left = invertTree(root->left);
+//        TreeNode* right = invertTree(root->right);
+//        root->left = right;
+//        root->right = left;
+//        return root;
+//    }
+//};
