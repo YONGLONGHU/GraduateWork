8b89d7940123bc97eaf1eaff28b6ed6e91c3e8a6
HUYONGLONG <2830915693@qq.com>
2025-01-04 13:30:23 +0800

    CCIL

--- GraduateWork/GraduateWork/力扣练习题.cpp
+++ GraduateWork/GraduateWork/力扣练习题.cpp
@@ -262,16 +262,16 @@
 //    }
 //};
 //·´×ª¶þ²æÊ÷
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr) {
-            return nullptr;
-        }
-        TreeNode* left = invertTree(root->left);
-        TreeNode* right = invertTree(root->right);
-        root->left = right;
-        root->right = left;
-        return root;
-    }
-};
+//class Solution {
+//public:
+//    TreeNode* invertTree(TreeNode* root) {
+//        if (root == nullptr) {
+//            return nullptr;
+//        }
+//        TreeNode* left = invertTree(root->left);
+//        TreeNode* right = invertTree(root->right);
+//        root->left = right;
+//        root->right = left;
+//        return root;
+//    }
+//};
