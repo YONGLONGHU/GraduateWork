d8754eb056578df101549718f722cc93c00cada3
HUYONGLONG <2830915693@qq.com>
2025-03-13 14:36:27 +0800

    初始化列表初始化顺序

--- GraduateWork/GraduateWork/力扣练习题.cpp
+++ GraduateWork/GraduateWork/力扣练习题.cpp
@@ -1269,3 +1269,23 @@ using namespace std;
 //{
 //	Date d1;
 //}
+class A
+{
+public:
+    A(int a)
+        :_a1(a)
+        , _a2(_a1)
+    {}
+
+    void Print() {
+        cout << _a1 << " " << _a2 << endl;
+    }
+private:
+    int _a2;
+    int _a1;
+};
+
+int main() {
+    A aa(1);
+    aa.Print();
+}
